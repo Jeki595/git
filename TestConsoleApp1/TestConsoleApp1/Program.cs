0baa1ff120a542cf1f5ed0b2717dc5745b2f209b
Jeki595 <jorikrika@gmail.com>
2023-03-28 09:37:04 +0300

    Создание веб приложения

--- TestConsoleApp1/TestConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("hi");
-Console.WriteLine("ey");
-Console.ReadLine();
-
-// Будущее поле ввода
