c04b90eb0df99e34f47967805f1e3ac6c9c0cbd1
Jeki595 <jorikrika@gmail.com>
2023-03-28 09:50:13 +0300

    Revert "Создание веб приложения"
    This reverts commit 0baa1ff120a542cf1f5ed0b2717dc5745b2f209b.

---
+++ TestConsoleApp1/TestConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("hi");
+Console.WriteLine("ey");
+Console.ReadLine();
+
+// Будущее поле ввода
